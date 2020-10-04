e77ce8962e3963146f21ead02205e729ae1198a6
TaoWang <tao.wang@realworld-one.com>
2020-10-04 12:53:20 +0200

    rename folder

--- Technical Test/Feature/Source/Feature/Feature.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Feature.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Feature, "Feature" );
-
-DEFINE_LOG_CATEGORY(LogFeature)
- 
