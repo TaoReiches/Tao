c62b2f1306053744c818e1472cdef3dc476a0333
wangtao <tao.wang.go@gmail.com>
2020-12-29 16:57:37 +0100

    Client begin loading.

--- TechnicalTest/NetTest/GameCommandExecute.h
+++ TechnicalTest/NetTest/GameCommandExecute.h
@@ -13,5 +13,27 @@ public:
         cmd.ParseFromString(command);
 
         std::cout << "Receive command: " << cmd.commandtype() << std::endl;
+
+        switch (cmd.commandtype())
+        {
+        case Game::TwGameCommandType::SC_CONNECT: BeginLoading(cmd.content()); break;
+        default:
+            break;
+        }
+    }
+
+private:
+    static void BeginLoading(std::string command)
+    {
+        Game::TwGameConnectionSC gameConnectionSC;
+        gameConnectionSC.ParseFromString(command);
+
+        std::cout << "Begin Loading: mapid - " << gameConnectionSC.mapid() <<
+            " unitTypeId - " << gameConnectionSC.herotypeid() <<
+            " userId - " << gameConnectionSC.userid() << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+        // send load end
+        std::cout << "After 3 seconds, send load end!" << std::endl;
     }
 };
